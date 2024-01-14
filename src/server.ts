58f3702cfbe37e3652685a45275b13c0d81ea003
Camila Couto <camilagcouto@hotmail.com>
2024-01-14 16:48:42 -0300

    primeiro commit do projeto

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+import Hapi from "@hapi/hapi";
+import { Server } from "@hapi/hapi";
+
+export let server: Server;
+
+export const init = async function(): Promise<Server> {
+    server = Hapi.server({
+        port: process.env.PORT || 3000,
+        host: '0.0.0.0'
+    });
+
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: (request: Request) => {
+            return "Hello World!.";
+        }
+    });
+
+    return server;
+};
+
+export const start = async function (): Promise<void> {
+    console.log(`Listening on ${server.settings.host}:${server.settings.port}`);
+    return server.start();
+};
+
+process.on('unhandledRejection', (err) => {
+    console.error("unhandledRejection");
+    console.error(err);
+    process.exit(1);
+});
