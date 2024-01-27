39dca8b77760a544e1ca11c9334a7abe678701a4
Camila Couto <camilagcouto@hotmail.com>
2024-01-27 20:08:27 -0300

    alteração host

--- src/infra/data/database/data-source.ts
+++ src/infra/data/database/data-source.ts
@@ -2,8 +2,8 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    // host: "mysql",
-    host: "localhost",
+    host: "mysql",
+    // host: "localhost",
     port: 3306,
     username: "fiap",
     password: "password",
