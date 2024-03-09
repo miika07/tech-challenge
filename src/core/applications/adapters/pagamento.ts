2888257a655ee7823d327b91ca33e74c0253fe0d
Melina Carniel <melinagarciac@gmail.com>
2024-03-09 00:58:49 -0300

    fix status

--- src/core/applications/adapters/pagamento.ts
+++ src/core/applications/adapters/pagamento.ts
@@ -1,11 +1,11 @@
 import { PagamentoEntity } from "../../domain/entities/pagamento";
-import { Pagamento } from "../models/pagamento";
+import { Pagamento, Status } from "../models/pagamento";
 
 export const parserPagamento = (pagamentoDB: PagamentoEntity) : Pagamento => {
     return {
         ...pagamentoDB.id && { id: pagamentoDB.id },
         idPedido: pagamentoDB.idPedido,
-        status: pagamentoDB.status
+        status: Status[pagamentoDB.status]
     }
 }
 
