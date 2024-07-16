c9eecd347d73966f6c7150382853b50018b5f4fd
Melina Carniel <melinagarciac@gmail.com>
2024-07-15 21:24:03 -0300

    tests produtos

--- __tests__/integration/methods/produto.test.ts
+++ __tests__/integration/methods/produto.test.ts
@@ -1,57 +1,100 @@
-// import { route, TestRouteOptions } from '../../common';
+import { route, TestRouteOptions } from '../../common';
+import nock from 'nock';
+import Config from '../../../src/config/environment.config'
 
-//   it('[GET] Buscar todos os produtos - 200', async () => {
-//     const params: TestRouteOptions = {
-//       method: 'GET',
-//       url: 'api/produtos',
-//       basePath: ''
-//     };
+const { url } = Config.apis.produtos;
+
+const mockResponse = [
+  {
+    id: "c4e53126-5a73-4ed0-b428-76950ed35b8c",
+    nome: "x-salada",
+    descricao: "Pão, salada, hamburguer, queijo",
+    preco: 22.50,
+    categoria: "Hamburguer"
+  },
+  {
+    id: "c4e53126-5a73-4ed0-b428-76950ed35djs",
+    nome: "Batata M",
+    descricao: "batata frita tamanho M",
+    preco: 10.50,
+    categoria: "Acompanhamentos"
+  }
+]
+
+  it('[GET] Buscar todos os produtos - 200', async () => {
+    const params: TestRouteOptions = {
+      method: 'GET',
+      url: 'api/produtos',
+      basePath: ''
+    };
+
+    nock(url)
+    .get('')
+    .reply(200, mockResponse);
     
-//     const { payload, statusCode } = await route(params);
-//     expect(statusCode).toBe(200);
-//     expect(payload).toHaveLength(1);
-//   });
+    const { payload, statusCode } = await route(params);
+    expect(statusCode).toBe(200);
+    expect(payload).toHaveLength(2);
+  });
 
-//   it('[GET] Buscar produto por ID - 200', async () => {
-//     const params: TestRouteOptions = {
-//       method: 'GET',
-//       url: 'api/produtos',
-//       basePath: ''
-//     };
+  it('[GET] Buscar produto por ID - 200', async () => {
+    const id = '12345';
+    const mockResponse = { id: '12345', nome: 'Produto Teste', preco: 99.99 };
+
+    const paramsId: TestRouteOptions = {
+      method: 'GET',
+      url: `api/produto/${id}`,
+      basePath: '',
+      query: {
+        id
+      }
+    };
     
-//     const response = await route(params);
-//     expect(response.statusCode).toBe(200);
+    nock(url)
+      .get(`/${id}`)
+      .reply(200, mockResponse);
 
-//     const paramsId: TestRouteOptions = {
-//       method: 'GET',
-//       url: `api/produto/${response.payload[0].id}`,
-//       basePath: '',
-//       query: {
-//         id:response.payload.id
-//       }
-//     };
-//     const { payload, statusCode } = await route(paramsId);
-//     expect(statusCode).toBe(200);
-//     expect(payload.nome).toBe('x-salada');
-//   });
+    const { payload, statusCode } = await route(paramsId);
+    expect(statusCode).toBe(200);
+    expect(payload.nome).toBe('Produto Teste');
+  });
 
-//   it('[GET] Buscar produto por Categoria - 200', async () => {
-//     const paramsId: TestRouteOptions = {
-//       method: 'GET',
-//       url: `api/produto/categoria/Lanches`,
-//       basePath: '',
-//     };
-//     const { payload, statusCode } = await route(paramsId);
-//     expect(statusCode).toBe(200);
-//     expect(payload[0].nome).toBe('x-salada');
-//   });
+  it('[GET] Buscar produto por Categoria - 200', async () => {
+    const categoria = 'Lanches';
+    
+    const paramsId: TestRouteOptions = {
+      method: 'GET',
+      url: `api/produto/categoria/Lanches`,
+      basePath: '',
+    };
+    
+    nock(url)
+      .get(`/categoria/${categoria}`)
+      .reply(200, mockResponse);
 
-//   it('[GET] Buscar produto por Categoria - 404', async () => {
-//     const paramsId: TestRouteOptions = {
-//       method: 'GET',
-//       url: `api/produto/categoria/Bebidas`,
-//       basePath: '',
-//     };
-//     const { payload, statusCode } = await route(paramsId);
-//     expect(statusCode).toBe(404);
-//   });
+    const { payload, statusCode } = await route(paramsId);
+    expect(statusCode).toBe(200);
+    expect(payload[0].nome).toBe('x-salada');
+  });
+
+  it('[GET] Buscar produto por Categoria - 404', async () => {
+    const categoria = 'Lanches';
+    
+    const paramsId: TestRouteOptions = {
+      method: 'GET',
+      url: `api/produto/categoria/Lanches`,
+      basePath: '',
+    };
+
+    const mockErrorResponse = { error: 'Not found' };
+
+    nock(url)
+      .get(`/categoria/${categoria}`)
+      .reply(404, mockErrorResponse);
+
+    try {
+      await route(paramsId);
+    } catch (error) {
+      expect(error.statusCode).toBe(404);
+    }
+  });
